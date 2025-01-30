1b5ec0a7f6d7a8eccc918dbd87e9e01b50680754
Junaid Ul Hassan <116207188+junaidulhassan@users.noreply.github.com>
2025-01-30 16:17:45 +0500

    Update api_token.py

--- Backend/api_token.py
+++ Backend/api_token.py
@@ -3,9 +3,9 @@ class LargeLanguageModel():
         pass
     
     def get_Key(self):
-        __api_key = "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"
+        __api_key = ""
         return __api_key
     
     def get_gpt_key(self):
-        __api_key = "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"
+        __api_key = '"
         return __api_key
